fc4fc743ab42d69e29564f831674d17dee8d1672
Saeid Alidadi <alidadisaeid@gmail.com>
2017-10-18 12:26:58 +0330

    finish: reducer composition with objects

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -34,4 +34,18 @@ const todos = (state = [], action) => {
 	}
 };
 
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+	switch (action.type) {
+		case 'SET_VISIBILITY_FILTER':
+			return action.filter;
+		default:
+			return state;
+	}
+};
 
+const todoApp = (state={}, action) => {
+	return {
+		todos: todos(state.todos, action),
+		visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+	}
+};
