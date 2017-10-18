210c93f89ee457d8d7ed9626dbf852643fd03b2f
Saeid Alidadi <alidadisaeid@gmail.com>
2017-10-18 10:08:22 +0330

    finish: react-counter-example

--- src/App.js
+++ src/App.js
@@ -2,54 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import * as expect from 'expect';
-import * as Redux from 'redux';
 
-
-
-
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state -1;
-    default:
-      return state;
-  }
-};
-
-const { createStore } = Redux;
-const store = createStore(counter);
-
-
-const render = () => {
-	document.getElementById("store").innerText = store.getState()
-};
-
-store.subscribe(render);
-render();
-document.addEventListener('click', () => {
-	store.dispatch({ type: 'INCREMENT'});
-})
-
-expect(
-  counter(0, { type: 'INCREMENT' })
-).toEqual(1);
-
-expect(
-	counter(1, { type: 'INCREMENT' })
-).toEqual(2);
-
-expect(
-	counter(0, { type: 'INCREMENT' })
-).toEqual(1);
-
-expect(
-	counter(1, { type: 'DECREMENT' })
-).toEqual(0);
+const Counter = ({ value, onIncrement, onDecrement }) =>
+  (
+    <div>
+      <h1>{value}</h1>
+      <button onClick={onIncrement}>INC</button>
+      <button onClick={onDecrement}>DEC</button>
+    </div>
+  );
 
 class App extends Component {
   render() {
+    const store = this.props.store;
     return (
       <div className="App">
         <header className="App-header">
@@ -59,6 +24,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Counter value={store.getState()}
+                 onIncrement={() =>
+                   store.dispatch({ type: 'INCREMENT'})
+                 }
+                 onDecrement={() =>
+	                 store.dispatch({ type: 'DECREMENT'})
+                 }
+        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as Redux from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const counter = (state = 0, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+			return state + 1;
+		case 'DECREMENT':
+			return state -1;
+		default:
+			return state;
+	}
+};
+
+const { createStore } = Redux;
+const store = createStore(counter);
+
+const render = () => {
+	ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+};
+
+store.subscribe(render);
+render();
+
+
 registerServiceWorker();
 
 
