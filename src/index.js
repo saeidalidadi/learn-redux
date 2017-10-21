fa2038ff9fca88350ead3d1407028d816559fe14
Saeid Alidadi <alidadisaeid@gmail.com>
2017-10-21 17:54:22 +0330

    finish: Passing the store down with react-redux

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import propTypes from 'prop-types';
+import {Provider} from 'react-redux'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './reducers';
-
-class Provider extends React.Component {
-	getChildContext() {
-		return {
-			store: this.props.store
-		}
-	}
-	render() {
-		return this.props.children
-	}
-}
-
-Provider.childContextTypes = {
-	store: propTypes.object
-};
 
 ReactDOM.render(
 	<Provider store={store}>
