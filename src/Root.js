06ed4bb14d2b795c9612411791cd77c8e19666ca
Saeid Alidadi <alidadisaeid@gmail.com>
2017-10-23 15:57:44 +0330

    finish: ading react router to project

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import {Provider} from 'react-redux'
+import {
+	BrowserRouter as Router,
+	Route
+} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import propTypes from 'prop-types';
 import App from './App';
 import './App.css';
 
 const Root = ({ store }) => {
 	return (
 		<Provider store={store}>
-			<App />
+			<Router>
+				<Route exact path='/' component={App}/>
+			</Router>
 		</Provider>
 	)
 };
+
+
+Root.propTypes = {
+	store: propTypes.object.isRequired
+}
 
 export default Root;
