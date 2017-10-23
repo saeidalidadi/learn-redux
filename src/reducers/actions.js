b8045bcde7ad05d8ef4fbd8dddb765aa2d4ebd95
Saeid Alidadi <alidadisaeid@gmail.com>
2017-10-23 10:24:28 +0330

    finish: simplifying the arrow functions

--- src/reducers/actions.js
+++ src/reducers/actions.js
@@ -1,24 +1,17 @@
 import v4 from 'uuid/v4';
 
-export const addTodo = (value) => {
-	return {
-		type: 'ADD_TODO',
-		text: value,
-		id: v4()
-	}
-};
+export const addTodo = (value) => ({
+	type: 'ADD_TODO',
+	text: value,
+	id: v4()
+});
 
-export const toggleTodo = (id) => {
-	return {
-		type: 'TOGGLE_TODO',
-		id
-	}
+export const toggleTodo = (id) => ({
+	type: 'TOGGLE_TODO',
+	id
+});
 
-};
-
-export const setVisibilityFilter = (filter) => {
-	return {
-		type: 'SET_VISIBILITY_FILTER',
-		filter
-	}
-};
+export const setVisibilityFilter = (filter) => ({
+	type: 'SET_VISIBILITY_FILTER',
+	filter
+});
