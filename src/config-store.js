9e22c12f6506d7a5501875bce8c93762bffb35ec
Saeid Alidadi <alidadisaeid@gmail.com>
2017-10-24 15:24:15 +0330

    finish: Normalizing the State Shape

--- src/config-store.js
+++ src/config-store.js
@@ -7,9 +7,8 @@ const { createStore } = Redux;
 
 export const store = createStore(todoReducers, loadState());
 
-
 store.subscribe(throttle(() =>
-	saveState({
-		todos: store.getState().todos
-	})
+  saveState({
+    todos: store.getState().todos
+  })
 ), 1000);
